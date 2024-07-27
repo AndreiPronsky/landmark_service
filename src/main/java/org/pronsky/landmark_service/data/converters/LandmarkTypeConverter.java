32beb805d99dcd3e26e8324d85b8b2f8c2ff3ab5
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2024-07-27 21:46:05 +0300

    Converter refactored

--- src/main/java/org/pronsky/landmark_service/data/converters/LandmarkTypeConverter.java
+++ src/main/java/org/pronsky/landmark_service/data/converters/LandmarkTypeConverter.java
@@ -7,26 +7,28 @@ public class LandmarkTypeConverter implements AttributeConverter<Landmark.Landma
 
     @Override
     public Integer convertToDatabaseColumn(Landmark.LandmarkType landmarkType) {
-        Integer id = null;
+        int id;
         switch (landmarkType) {
-           case ARCHITECTURE -> id = 1;
-           case         MUSEUMS_AND_GALLERIES -> id = 2;
-           case         BEACHES_AND_PARKS -> id = 3;
-           case         ARCHEOLOGICAL -> id = 4;
-           case         HISTORICAL -> id = 5;
+            case ARCHITECTURE -> id = 1;
+            case MUSEUMS_AND_GALLERIES -> id = 2;
+            case BEACHES_AND_PARKS -> id = 3;
+            case ARCHEOLOGICAL -> id = 4;
+            case HISTORICAL -> id = 5;
+            default -> throw new IllegalArgumentException("Invalid landmarkType: " + landmarkType);
         }
         return id;
     }
 
     @Override
     public Landmark.LandmarkType convertToEntityAttribute(Integer typeId) {
-        Landmark.LandmarkType type = null;
+        Landmark.LandmarkType type;
         switch (typeId) {
             case 1 -> type = Landmark.LandmarkType.ARCHITECTURE;
             case 2 -> type = Landmark.LandmarkType.MUSEUMS_AND_GALLERIES;
             case 3 -> type = Landmark.LandmarkType.BEACHES_AND_PARKS;
             case 4 -> type = Landmark.LandmarkType.ARCHEOLOGICAL;
             case 5 -> type = Landmark.LandmarkType.HISTORICAL;
+            default -> throw new IllegalArgumentException("Invalid typeId: " + typeId);
         }
         return type;
     }
